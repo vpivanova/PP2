eb8547965de257688af0d2ccc15b50d9acf39fa1
Veronika <Nik_name27@mail.ru>
2025-05-13 21:53:41 +0600

    client debag

--- src/app/_components/squad/grade.tsx
+++ src/app/_components/squad/grade.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { CheckIcon } from "@heroicons/react/24/outline";
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { api } from "../../../trpc/react";
 
 export function Grade({
@@ -17,8 +17,24 @@ export function Grade({
   const grades = api.grade.getByTask.useQuery({ taskId });
   const set = api.grade.create.useMutation();
   console.log(grades.data);
+
+  const [lastGrade, setLastGrade] = useState<number | null>(null); // Измените на null
+
+  useEffect(() => {
+    const studentGrade = grades.data?.find(grade => grade.studentId === studentId);
+    if (studentGrade) {
+      setLastGrade(studentGrade.value);
+    } 
+  }, [grades.data, studentId]);
+
+  // const onSetHandler = () => {
+  //   set.mutate({ taskId, studentId, value: Number(valRef.current?.value), squadTutorId: squadTutorId, });
+  // };
   const onSetHandler = () => {
-    set.mutate({ taskId, studentId, value: Number(valRef.current?.value), squadTutorId: squadTutorId, });
+    set.mutate({
+      taskId, studentId, value: Number(valRef.current?.value),
+      squadTutorId: ""
+    });
   };
 
   return (
@@ -27,7 +43,9 @@ export function Grade({
         ref={valRef}
         type="number"
         name="value"
-        defaultValue={0}
+        value={lastGrade !== null ? lastGrade : ''} // Отображаем значение или пустую строку
+        onChange={(e) => setLastGrade(Number(e.target.value))}
+        //defaultValue={0}
         className="w-8 px-px"
       />
       <button type="submit" onClick={onSetHandler}>
--- src/app/_components/squad/students.tsx
+++ src/app/_components/squad/students.tsx
@@ -24,9 +24,15 @@ export function Students({
   const grades = api.grade.getByTask.useQuery({ taskId }).data ?? [];
   const r = api.user.getByQuery.useQuery({ query: search }).data;
 
-  const studentsWithGrades = students.map((s) => {
-    const g = grades.find((g) => g.studentId === s.id);
-    return { ...s, value: g?.value ?? 0 };
+  // const studentsWithGrades = students.map((s) => {
+  //   const g = grades.find((g) => g.studentId === s.id);
+  //   return { ...s, value: g?.value ?? 0 };
+  const studentsWithGrades = students.map((student) => {
+    const studentGrades = grades.filter((g) => g.studentId === student.id);
+
+    const lastGrade = studentGrades.length > 0 ? studentGrades[studentGrades.length - 1] : null;
+  
+    return { ...student, value: lastGrade?.value ?? 0 };
   });
   console.log(studentsWithGrades);
 
